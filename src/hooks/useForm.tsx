9eff04ccfe05ae6450bb008f8aa6b9d196829f6c
Saumay Paul <saumay@chainflux.com>
2022-07-03 22:39:54 +0530

    added form validtion file

---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,100 @@
+import { omit } from 'lodash'
+import React, { useState } from 'react'
+
+interface Props {
+    callback: any
+}
+
+const useForm = (callback: any) => {
+    //Form values
+    interface IValues {
+        [key: string]: any
+    }
+    const [values, setValues] = useState<IValues>({})
+    //Errors
+    interface IErrors {
+        [key: string]: any
+    }
+    const [errors, setErrors] = useState<IErrors>({})
+
+    //A method to handle form inputs
+    const handleChange = (event: any) => {
+        //To stop default events
+        event.persist()
+
+        const name = event.target.name
+        const val = event.target.value
+
+        validate(event, name, val)
+        //Let's set these values in state
+
+        setValues({
+            ...values,
+            [name]: val,
+        })
+    }
+
+    const handleSubmit = (event: any) => {
+        if (event) event.preventDefault()
+
+        if (
+            Object.keys(errors).length === 0 &&
+            Object.keys(values).length !== 0
+        ) {
+            callback()
+        } else {
+            alert('There is an Error!')
+        }
+    }
+
+    const validate = (event: any, name: any, value: any) => {
+        //A function to validate each input values
+
+        let newObj: any
+        switch (name) {
+            case 'email':
+                if (
+                    !new RegExp(
+                        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                    ).test(value)
+                ) {
+                    setErrors({
+                        ...errors,
+                        email: 'Enter a valid email address',
+                    })
+                } else {
+                    newObj = omit(errors, 'email')
+                    setErrors(newObj)
+                }
+                break
+
+            case 'password':
+                if (
+                    !new RegExp(
+                        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/
+                    ).test(value)
+                ) {
+                    setErrors({
+                        ...errors,
+                        password:
+                            'Password should contains atleast 8 charaters and containing uppercase,lowercase and numbers',
+                    })
+                } else {
+                    newObj = omit(errors, 'password')
+                    setErrors(newObj)
+                }
+                break
+
+            default:
+                break
+        }
+    }
+    return {
+        values,
+        errors,
+        handleChange,
+        handleSubmit,
+    }
+}
+
+export default useForm
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -12,95 +12,109 @@ import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { AUTH_ACTIONS } from '../../redux/actions/auth.action'
 import { pathNames } from '../../routes/pathNames'
+import useForm from '../../hooks/useForm'
 
 interface Props {}
 
 const Login: React.FC<Props> = (props: Props) => {
-  const dispatch = useDispatch()
+    const login = () => {
+        dispatch(AUTH_ACTIONS.login({ roles: ['ISSUER'] }))
+        navigate(pathNames.DASHBOARD, { replace: true })
+    }
 
-  const navigate = useNavigate()
-  const login = () => {
-    dispatch(AUTH_ACTIONS.login({ roles: ['ISSUER'] }))
-    navigate(pathNames.DASHBOARD, { replace: true })
-  }
+    const { handleChange, values, errors, handleSubmit } = useForm(login)
 
-  return (
-    // <div data-testid={"loginBtn"} onClick={() => login()}>
-    //   Login Me in to React
-    // </div>
-    <Grid container component="main" sx={{ height: '100vh' }}>
-      <Grid item xs={12} component={Paper} elevation={6} square>
-        <Box
-          sx={{
-            my: 8,
-            mx: 4,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Typography component="h1" variant="h5">
-            Carbo Credit Sign in
-          </Typography>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              data-testid={'loginBtn'}
-              onClick={() => login()}
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{
-                mt: 3,
-                mb: 2,
-                color: 'primary.light',
-                textTransform: 'none',
-              }}
-              color="primary"
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
+    const dispatch = useDispatch()
+
+    const navigate = useNavigate()
+
+    return (
+        <Grid container component="main" sx={{ height: '100vh' }}>
+            <Grid item xs={12} component={Paper} elevation={6} square>
+                <Box
+                    sx={{
+                        my: 8,
+                        mx: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography component="h1" variant="h5">
+                        Carbon Credit Sign in
+                    </Typography>
+                    <Box
+                        component="form"
+                        noValidate
+                        sx={{ mt: 1 }}
+                        onSubmit={handleSubmit}
+                    >
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            defaultValue={values?.email}
+                            autoComplete="email"
+                            autoFocus
+                            onChange={handleChange}
+                            error={errors?.email}
+                        />
+
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            defaultValue={values?.password}
+                            autoComplete="current-password"
+                            onChange={handleChange}
+                            error={errors?.password}
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox value="remember" color="primary" />
+                            }
+                            label="Remember me"
+                        />
+                        <Button
+                            data-testid={'loginBtn'}
+                            // onClick={handleSubmit}
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{
+                                mt: 3,
+                                mb: 2,
+                                color: 'primary.light',
+                                textTransform: 'none',
+                            }}
+                            color="primary"
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
             </Grid>
-            {/* <Copyright sx={{ mt: 5 }} /> */}
-          </Box>
-        </Box>
-      </Grid>
-    </Grid>
-  )
+        </Grid>
+    )
 }
 
 export default Login
