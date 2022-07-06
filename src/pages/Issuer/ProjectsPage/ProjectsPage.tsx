f0d34b4447148f470ff0b81939863579128c7bdc
Saumay Paul <saumay@chainflux.com>
2022-07-06 15:30:15 +0530

    issuer pages

---
+++ src/pages/Issuer/ProjectsPage/ProjectsPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { ProjectsPageProps } from './ProjectsPage.interface'
+
+const ProjectsPage = (props: ProjectsPageProps) => {
+    return <div>ProjectsPage</div>
+}
+
+export default ProjectsPage
