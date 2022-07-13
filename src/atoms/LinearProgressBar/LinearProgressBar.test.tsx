fb7e1d176907e0d01738bce8d9badb9cd34233d3
Saumay Paul <saumay@chainflux.com>
2022-07-13 11:46:19 +0530

    fixes in view

---
+++ src/atoms/LinearProgressBar/LinearProgressBar.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import LinearProgressBar from './LinearProgressBar';
+
+test("renders LinearProgressBar", () => {
+  // render(<App />);
+  // const linkElement = screen.getByText(/LinearProgressBar/i);
+  // expect(linkElement).toBeInTheDocument();
+});
