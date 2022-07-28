cb323ca031259e2d6af159aaacd964e3a2da99ac
avadhesh1 <avadhesh@chainflux.com>
2022-07-28 13:59:11 +0530

    Design UI of Section E1 to E7

---
+++ src/atoms/SampleModal/SampleModal.tsx
@@ -0,0 +1,95 @@
+import React, { FC, useState } from 'react'
+import {
+  Box,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Typography,
+  Modal,
+} from '@mui/material'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+
+interface SampleModalProps {
+  mediaArray: Array<any>
+  stringArray: Array<string>
+  modalVisibility: boolean
+  setModalVisibility: any
+}
+
+const SampleModal: FC<SampleModalProps> = (props: SampleModalProps) => {
+  const [index, setIndex] = useState(0)
+
+  const counterUp = () => {
+    if (index < props.mediaArray.length - 1) {
+      setIndex(index + 1)
+    }
+  }
+
+  const counterDown = () => {
+    if (index > 0) {
+      setIndex(index - 1)
+    }
+  }
+
+  return (
+    <Modal
+      open={props.modalVisibility}
+      onClose={() => props.setModalVisibility(false)}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+      // sx={{ width:  '1000px' }}
+    >
+      <Box
+        sx={{
+          width: '100%',
+          height: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          display: 'flex',
+          zIndex: 0,
+        }}
+        // onClick={() => props.setModalVisibility(false)}
+      >
+        <Box sx={{ backgroundColor: '#FFF', borderRadius: '5px', zIndex: 100, padding: '10px', paddingRight: '5px', paddingLeft: '5px' }}>
+          <Typography>{props.stringArray[index]}</Typography>
+          <Box
+            sx={{ height: '400px', marginTop: '4px', marginBottom: '16px' }}
+            component={'img'}
+            src={props.mediaArray[index]}
+          />
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <ChevronLeftIcon onClick={counterDown} />
+            <Typography
+              sx={{
+                display: 'flex',
+                height: '30px',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Typography
+                sx={{
+                  height: '30px',
+                  width: '30px',
+                  backgroundColor: '#E8E8E8',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: '5px',
+                }}
+              >
+                {index + 1}
+              </Typography>
+              {' ' + '/ ' + props.mediaArray.length}
+            </Typography>
+            <ChevronRightIcon onClick={counterUp} />
+          </Box>
+        </Box>
+      </Box>
+    </Modal>
+  )
+}
+
+export default SampleModal
