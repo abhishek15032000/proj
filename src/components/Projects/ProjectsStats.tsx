dff6fd8dd0a5f19f0e281844fcde0ef3d6fcd197
basava-chainflux <basava@chainflux.com>
2022-08-08 19:21:53 +0530

    Fixes

--- src/components/Projects/ProjectsStats.tsx
+++ src/components/Projects/ProjectsStats.tsx
@@ -10,14 +10,35 @@ const stats = [
     title: 'Ongoing registration of projects',
     value: 3,
   },
-  // {
-  //   title: 'Total verified',
-  //   value: 3,
-  // },
+  {
+    title: 'Total verified',
+    value: 3,
+  },
+  {
+    title: 'Total registered projects',
+    value: 8,
+  },
+  {
+    title: 'Ongoing registration of projects',
+    value: 3,
+  },
+  {
+    title: 'Total verified',
+    value: 3,
+  },
 ]
 const ProjectsStats = () => {
   return (
-    <Box sx={{ mt: 3 }} className="stats-row">
+    <Box
+      sx={{
+        mt: 3,
+        overflowX: 'auto',
+        maxWidth: '100%',
+        margin: 1,
+        // height: '300px'
+      }}
+      className="stats-row"
+    >
       {stats?.map((stat, index) => (
         <Box key={index} className="stats-container">
           <Box className="content-container">
