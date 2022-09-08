f9e62bbf107a64a7ecabd8e5a89db6b8b16e15c7
swapnil-chainflux <swapnil@chainflux.com>
2022-09-08 23:17:43 +0530

    unit test cases for CCTable, CCTableSkeleton and Logo components

--- src/atoms/CCSelectBox/CCSelectBox.test.tsx
+++ src/atoms/CCSelectBox/CCSelectBox.test.tsx
@@ -1,9 +1,56 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import CCSelectBox from './CCSelectBox';
+import { fireEvent, render, screen } from '@testing-library/react'
+import CCSelectBox from './CCSelectBox'
+import { ROLES } from '../../config/roles.config'
 
-test("renders CCSelectBox", () => {
-  // render(<App />);
-  // const linkElement = screen.getByText(/CCSelectBox/i);
-  // expect(linkElement).toBeInTheDocument();
-});
+const typeOptions = [
+  { name: 'Issuer', value: ROLES.ISSUER },
+  { name: 'Verifier', value: ROLES.VERIFIER },
+]
+test('renders CCSelectBox', () => {
+//   render(
+//     <CCSelectBox
+//       native={true}
+//       label="Particpant Type"
+//       items={typeOptions}
+//       // onChange={(e) => setSelectedRole(e.target.value)}
+//     />
+//   )
+//   const elem = screen.getByRole('option', {
+//     name: 'Issuer',
+//   }) as HTMLSelectElement
+
+//   // expect(
+//   //   screen.getByRole('option', { name: 'Select a country' }).selected
+//   // ).toBe(true)
+//   // if (elem) expect(elem.selected).toBe(true)
+})
+// it('should display the correct number of options', () => {
+//   render(
+//     <CCSelectBox
+//       label="Particpant Type"
+//       items={typeOptions}
+//       // onChange={(e) => setSelectedRole(e.target.value)}
+//     />
+//   )
+//   expect(screen.getAllByRole('option').length).toBe(2)
+// })
+// it('calls onChange if change event fired', () => {
+//   const mockCallback = jest.fn()
+//   const { getByTestId } = render(
+//     <div>
+//       <CCSelectBox
+//         label="Particpant Type"
+//         items={typeOptions}
+//         data-testid="my-wrapper"
+//         onChange={mockCallback}
+//       />
+//     </div>
+//   )
+//   const wrapperNode = getByTestId('my-wrapper')
+//   console.log(wrapperNode)
+//   // Dig deep to find the actual <select>
+//   const selectNode = wrapperNode.childNodes[0].childNodes[0]
+//   fireEvent.change(selectNode, { target: { value: '3' } })
+//   expect(mockCallback.mock.calls).toHaveLength(1)
+// })
