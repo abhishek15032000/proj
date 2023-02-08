0e46074ad4e0b3572024b71d8b4d3ed34bd0c1ba
swapnil-chainflux <swapnil@chainflux.com>
2023-02-08 12:34:17 +0530

    Removed token and contracts and Wallet from nav

--- src/components/Appbar/Drawer/MenuList.ts
+++ src/components/Appbar/Drawer/MenuList.ts
@@ -3,7 +3,7 @@ import { linkLabels } from '../../../routes/pathNames'
 const MENUS = {
   issuer_menus: [
     linkLabels.Dashboard,
-    linkLabels.Token_Contract,
+    // linkLabels.Token_Contract,
     // linkLabels.My_Portfolio,
     linkLabels.Marketplace,
     linkLabels.Wallet,
